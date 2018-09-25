a5301e6344b4f619ebd5ca0b0bfd4eaaa05fc1a9
Orion <32593795+angus-y@users.noreply.github.com>
2018-09-25 20:10:15 +0800

    Update registry_persist.py

--- components/windows/registry_persist.py
+++ components/windows/registry_persist.py
@@ -8,8 +8,7 @@ def main(option):
     if option == 'generate':
         config.import_statements.append('import _winreg')
         config.import_statements.append('from sys import argv')
-        config.import_statements.append('from os import getcwd')
-        config.import_statements.append('from os import path')
+        config.import_statements.append('from os import getcwd, path')
         config.functions.append('''
 def registry_persist(path):
     try:
